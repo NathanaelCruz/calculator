651605c4dd96d692ab4b3ab350853f50eeaeb31a
Nathanael Cruz <nathanael.cruz.fd@gmail.com>
2021-02-01 16:39:05 -0300

    Add struct based calculator

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Calculadora from "./main/Calculator"
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <h1>Calculadora</h1>
+    <Calculadora />
+  </>,
   document.getElementById('root')
 );
 
